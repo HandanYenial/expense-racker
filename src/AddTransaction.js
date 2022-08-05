fe2d78cf53a1a30cebd335b7970226fb1147cfb3
Your Name <you@example.com>
2022-08-04 22:33:35 -0400

    new commit

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -1,53 +1,43 @@
-import React from 'react'
-import { useState } from 'react';
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from './GlobalState';
 
-// we will create a piece of state, let's call it text then we will write a 
-//function to manipulate that piece of state let's call it setText and we want to
-//set this to use state and it will be initially an empty string, because in Add transaction the
-//input will be text-a string.
+export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
 
-//Then I will do the same thing for the amount(in AddTransaction i have atext input
-//and an amount input which is a number. So initially the amount input will be a number as 0)
+  const { addTransaction } = useContext(GlobalContext);
 
+  const onSubmit = e => {
+    e.preventDefault();
 
-function AddTransaction() {
-    const [text,setText] = useState('');
-    const[amount,setAmount] = useState(0);
-// Then I want to connect those states to my input: text and amount.
-//   So I can start with adding value to my input as value={text} and value= {amount}
-//Whenever the user enters a text or amount to the form, it needs to update the data--so I 
-//need a function to update the text and amount value whenever there is a change. which is onChange
-//in the imput field I need onChange = {(e) => setText(e.target.value)} .target.value will give us
-//whatever the user types in the input field.
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
 
   return (
-    <div>
-        <h3> Add new transaction </h3>
-        <form>
-            <div className="form-control">
-                <label htmlFor="text"> Text </label>
-                <input 
-                      type="text"                    
-                      placeholder="Enter text.."
-                      value={text}
-                      onChange={(e) => setText(e.target.value)}
-                />
-            </div>
-            <div className="form-control">
-                <label htmlFor="amount">
-                    Amount (negative-expense , positive-income)
-                </label>
-                <input 
-                        type="number"
-                        placeholder="Enter amount..."
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                />            
-            </div>
-            <button className="btn">Add Transaction</button>
-        </form>
-    </div>
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
   )
 }
 
-export default AddTransaction
+export default AddTransaction;
--- src/TransactionList.js
+++ src/TransactionList.js
@@ -1,17 +1,21 @@
 import React from 'react'
+//pull global state
+import { GlobalContext } from "./GlobalState";
+import {useContext } from "react";
+import  Transaction  from "./Transaction";
+
 
 function TransactionList() {
+    const { transactions } = useContext(GlobalContext);
   return (
     <div>
         <h3> History </h3>
         <ul id="list" className="list">
-            <li className="minus">
-                Cash <span> -$400 </span>
-                <button className="delete-btn"> x </button>
-            </li>
+          {transactions.map(transaction => (<Transaction key = {transaction.id} transaction = {transaction} /> ))}
+ 
         </ul>
     </div>
   )
 }
 
-export default TransactionList
+export default TransactionList;
